ffb77fd8e392ca66108ea48c0c59d37ab5b092e0
MohammedAbuEssa <mabuissa5@gmail.com>
2023-07-10 12:11:35 +0000

    Add messageQueue

--- system/system.js
+++ system/system.js
@@ -1,20 +1,28 @@
 "use strict";
-
 require("dotenv").config();
 const port = process.env.PORT;
 const ioServer = require("socket.io")(port);
-const { faker } = require("@faker-js/faker");
+
+let queue = {
+  flights: {},
+};
 
 ioServer.on("connection", (socket) => {
   console.log(`connected with ${socket.id}`);
   socket.on("new-flight", (payload) => {
+    let id = payload.id;
+    queue.flights[id] = payload;
     let obj = {
       event: `new flight `,
       time: new Date(),
+      id: id,
       Details: payload,
     };
     console.log(obj);
     ioServer.emit("schedialed", payload);
+  });
+  socket.on("delete", (payload) => {
+    delete queue.flights[payload.id];
   });
   socket.on("Arrived", (payload) => {
     let obj = {
@@ -24,6 +32,12 @@ ioServer.on("connection", (socket) => {
     };
     console.log(obj);
     ioServer.emit("ArrivedM", payload);
+  });
+  socket.on("get_all", () => {
+    Object.keys(queue.flights).map((id) => {
+      socket.emit("flight", (id, queue.flights[id]));
+      // console.log(id);
+    });
   });
 });
 
