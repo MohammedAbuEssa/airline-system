1658e8cb8b229cf270063a5120fe9402b45e2151
MohammedAbuEssa <mabuissa5@gmail.com>
2023-07-10 11:54:19 +0000

    Fix issues

--- moduler/system/system.js
+++ moduler/system/system.js
@@ -1,77 +1,43 @@
 "use strict";
+
 require("dotenv").config();
-// const event = require("../events");
-// const pilotName = require("../manager/manager");
-// const { faker } = require("@faker-js/faker");
-
-const port = process.env.PORT || 3030;
+const port = process.env.PORT;
 const ioServer = require("socket.io")(port);
-
-// require("../manager/manager.js");
-// require("../pilot/pilot.js");
-
-//New
-const airline = ioServer.of("/airline");
+const { faker } = require("@faker-js/faker");
 
 ioServer.on("connection", (socket) => {
-  console.log("connected", socket.id);
-  socket.on("arrived", (payload) => {
-    ioServer.emit("arrivedM", payload);
-  });
+  console.log(`connected with ${socket.id}`);
   socket.on("new-flight", (payload) => {
-    ioServer.emit("flight", payload);
-    console.log("Flight:");
-    console.log({
-      event: "new-flight",
-      time: new Date().toLocaleString(),
+    let obj = {
+      event: `new flight `,
+      time: new Date(),
       Details: payload,
-    });
+    };
+    console.log(obj);
+    ioServer.emit("schedialed", payload);
   });
-  // airline.emit("new-flight", socket);
+  socket.on("Arrived", (payload) => {
+    let obj = {
+      event: `arrived `,
+      time: new Date(),
+      Details: payload,
+    };
+    console.log(obj);
+    ioServer.emit("ArrivedM", payload);
+  });
 });
+
+const airline = ioServer.of("/airline");
 
 airline.on("connection", (socket) => {
-  console.log(`connection to airline ${socket.id}`);
+  console.log(`connected the airline with ${socket.id}`);
   socket.on("took-off", (payload) => {
-    airline.emit("arrived", payload);
+    let obj = {
+      event: `took off`,
+      time: new Date(),
+      Details: payload,
+    };
+    console.log(obj);
+    socket.emit("tookOff", payload);
   });
 });
-
-// ioServer.on("took-off", (payload) => {
-//   console.log("Flight:");
-//   console.log({
-//     event: "took_off",
-//     time: new Date().toLocaleString(),
-//     Details: payload,
-//   });
-// });
-
-// ioServer.on("arrived", (payload) => {
-//   console.log("Flight:");
-//   console.log({
-//     event: "arrived",
-//     time: new Date().toLocaleString(),
-//     Details: payload,
-//   });
-// });
-
-// //New
-
-// setInterval(() => {
-//   const flightID = faker.string.uuid();
-//   const pilotName = `${faker.person.firstName()} ${faker.person.lastName()}`;
-//   const destination = faker.location.city();
-
-//   const payload = {
-//     airLine: "Royal Jordanian Airlines",
-//     flightID,
-//     pilot: pilotName,
-//     destination,
-//   };
-
-//   ioServer.emit("new-flight", payload);
-// }, 10000);
-
-// console.log(
-//   `Manager: we’re greatly thankful for the amazing flight, ${pilotName.pilotFirstName} ${pilotName.pilotSecondtName}`
-// );
